dc8048b8841d8440d7d3ac5be9ac8caced65d0e5
omerhashmininjago <hashmi.omer@gmail.com>
2019-02-08 02:57:49 +0530

    Implementation

--- persistence/src/main/java/com/file/persistence/repo/CashflowRepo.java
+++ persistence/src/main/java/com/file/persistence/repo/CashflowRepo.java
@@ -2,6 +2,8 @@ package com.file.persistence.repo;
 
 import com.file.persistence.domain.Cashflow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CashflowRepo extends JpaRepository<Cashflow, String> {
 }
